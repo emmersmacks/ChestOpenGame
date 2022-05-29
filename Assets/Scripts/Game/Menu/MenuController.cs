4656063c8540e5baf1cd5c2505ff8d8e16c9a063
Emmersmacks <visnevskijan@gmail.com>
2022-05-29 20:15:51 +0300

    F! add switch screen animation

--- Assets/Scripts/Game/Menu/MenuController.cs
+++ Assets/Scripts/Game/Menu/MenuController.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MenuController<T, U> : Controller<T, U> where T : MenuView where U : MenuModel
 {
     public MenuController(T view, U model) : base(view, model) { }
+
+    private bool isAnimationStart = false;
 
     protected override void Init()
     {
@@ -19,21 +25,23 @@ public class MenuController<T, U> : Controller<T, U> where T : MenuView where U 
         _model.currentScreen = _view.chestInventoryScreen;
         _view.chestInventoryScreen.SetActive(true);
         _model.currentButton = _view.chestInventoryButton.gameObject;
+        ShowScreen(_model.currentScreen.GetComponent<CanvasGroup>());
         MarkButtonEnabled(_view.chestInventoryButton);
     }
 
     private void StartMenuSwitch(GameObject screen, Button button)
     {
-        MarkButtonDisable();
-        MarkButtonEnabled(button);
-        SwitchScreens(screen);
+        if(isAnimationStart == false)
+        {
+            MarkButtonDisable();
+            MarkButtonEnabled(button);
+            SwitchScreens(screen);
+        }
     }
 
-    public void SwitchScreens(GameObject screen)
+    public async UniTask SwitchScreens(GameObject screen)
     {
-        if (_model.currentScreen != null)
-            _model.currentScreen.SetActive(false);
-        screen.SetActive(true);
+        await StartAlphaSwitchAnimation(_model.currentScreen.GetComponent<CanvasGroup>(), screen.GetComponent<CanvasGroup>());
         _model.currentScreen = screen;
     }
 
@@ -48,5 +56,31 @@ public class MenuController<T, U> : Controller<T, U> where T : MenuView where U 
     {
         _model.currentButton.transform.GetComponent<Image>().sprite = _model.buttonDisabled;
         _model.currentButton.transform.GetChild(0).GetComponent<Image>().color = Color.black;
+    }
+
+    private async UniTask StartAlphaSwitchAnimation(CanvasGroup currentGroup, CanvasGroup nextGroup)
+    {
+        isAnimationStart = true;
+        await HideScreen(currentGroup);
+        await ShowScreen(nextGroup);
+        isAnimationStart = false;
+    }
+
+    private async UniTask HideScreen(CanvasGroup screen)
+    {
+        while (screen.alpha > 0)
+        {
+            screen.alpha -= 0.05f;
+            await UniTask.Delay(10);
+        }
+    }
+
+    private async UniTask ShowScreen(CanvasGroup screen)
+    {
+        while (screen.alpha < 1)
+        {
+            screen.alpha += 0.05f;
+            await UniTask.Delay(10);
+        }
     }
 }
